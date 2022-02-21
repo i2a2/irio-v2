2e6f3e62d57223913457f959676af814940bd29a
jcvillanuevamar <josvilmar99@gmail.com>
2022-02-21 16:57:49 +0000

    First GoogleTest and some useful libraries. Functionality not used commented

--- src/test/c++/irioTests/TP_onlyResources.cpp
+++ src/test/c++/irioTests/TP_onlyResources.cpp
@@ -7,47 +7,36 @@
 #include <boost/version.hpp>
 #include <boost/regex.hpp>
 
-// IRIO Library
-#include <irioDriver.h>
-#include <irioDataTypes.h>
-#include <irioHandlerAnalog.h>
-#include <irioHandlerDigital.h>
-#include <irioHandlerDMA.h>
-#include <irioHandlerImage.h>
-#include <irioHandlerSG.h>
-#include <irioError.h>
-#include <irioResourceFinder.h>
-
-
 /**
- * Test verifies driver’s ability to read resources in the FPGA.
+ * Test verifies driver’s ability to read and testing resources in the FPGA.
  * This test is related to the following requirements:
  *
  * PXIE7966R
  * PXIE7965R
  * The execution of this test requires to have an environment variable indicating the
  * serial number of the RIO board to be used. Execute in a command shell the lsrio.py command
- * execute export RIOserial=0x..........
+ * execute export RIOSerial="0x.........."
  */
 
-
-TEST(TP_NID_1000, CPUDAQ)
+TEST(TP_onlyResources, testCPUDAQ)
 {
 
 }
 
-TEST(TP_NID_1000, CPUIMAQ)
+TEST(TP_onlyResources, testCPUIMAQ)
 {
 
 }
 
-TEST(TP_NID_1000, MAXIO)
+TEST(TP_onlyResources, testMAXIO)
 {
 
 }
 
-TEST(TP_NID_1000, missingResources)
+TEST(TP_onlyResources, testmissingResources)
 {
 
 }
+
+
 
