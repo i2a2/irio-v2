eb277077a79b15408d2d5138740de6b328d5a196
jcvillanuevamar <josvilmar99@gmail.com>
2022-03-28 13:14:11 +0200

    New bitfiles and tests for CompacRIO modules and renamed FlexRIO files for more clarity

---
+++ src/test/c++/irioTests/TP_cRIO_PBP.cpp
@@ -0,0 +1,38 @@
+#include <gtest/gtest.h>
+
+#include <cstdlib>
+
+// IRIO Library
+extern "C" {
+#include <irioDriver.h>
+#include <irioDataTypes.h>
+#include <irioHandlerAnalog.h>
+#include <irioHandlerDigital.h>
+#include <irioHandlerSG.h>
+#include <irioError.h>
+#include <irioResourceFinder.h>
+}
+
+#include "testUtils-IRIO.h"
+
+// Always max verbosity
+static int verbosity = 1;
+
+using std::cout; using std::endl;
+using std::string;
+
+/*
+ * Test verifies driver’s ability to test cRIO PBP (Point by Point) Profile.
+ *
+ * This test is related to the following requirements:
+ *
+ * NI9159 Chassis
+ * 9264, 9205, 9401, 9477, 9426, 9476 and 9425 cRIO modules
+ *
+ * This test is executed only with 9205, 9401, 9477 and 9426 modules
+ *
+ */
+
+TEST(TP_cRIO_PBP, functional) {
+
+}
