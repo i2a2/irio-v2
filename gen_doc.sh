282b09b0165a5ea5c04f1669bc6b47a57de48020
Jc Villanueva <josvilmar99@gmail.com>
2022-06-14 15:28:15 +0200

    Bash script to automatize documentation generation. Execute from project root directory

--- gen_doc.sh
+++ gen_doc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 mvn package
-cd target/doc/irio-v2
-sed -i 's/IMAGE_PATH             =/IMAGE_PATH             = ./g' doxygen.config
+cd target/doc/irio
 doxygen doxygen.config
 cd -
-firefox target/doc/irio-v2/html/index.html &
-
+firefox target/doc/irio/html/index.html &
