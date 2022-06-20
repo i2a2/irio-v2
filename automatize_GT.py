1269a233460c0e3eaa972b009b16535ae1e440b9
Jc Villanueva <josvilmar99@gmail.com>
2022-06-20 18:02:00 +0200

    Python script to automatize GoogleTests execution

--- automatize_GT.py
+++ automatize_GT.py
@@ -1,6 +1,7 @@
 #!/usr/bin python3 
 import os   
 import sys 
+import argparse
 
 # It is mandatory to compile the project before execute this script
 #os.system("mvn clean compile -D coverage")
@@ -12,57 +13,40 @@ iterations = " --gtest_repeat=1"
 
 shuffleAndBreak = " --gtest_break_on_failure --gtest_shuffle"
 
-allTests  = sys.argv[1]
-RIOSerial = sys.argv[2]
-RIODevice = sys.argv[3]
+parser = argparse.ArgumentParser(prog="automatize_GT.py",description='Declare environment variables to automatize GoogleTests execution.')
 
-if (RIODevice == "7966" and RIOSerial == "0x0177A2AD"):   
-    Coupling = sys.argv[4]
-    
+parser.add_argument('--RIODevice',help='RIO device model. Use $lsrio command to display it',choices=['7961','7965','7966','7975','9159'],required='True')
+parser.add_argument('--RIOSerial',help='RIO device serial number. Use $lsrio command to display it',required='True')
+parser.add_argument('--allTests',help='Decide if run only funcional tests or funcional tests and error tests. 0 functional ; 1 functional+errors',choices=['1','0'],default='0')
+parser.add_argument('--AM',help='Indicate which adapter module is used. It is mandatory that the adapter module match with the RIODevice and RIOSerial used.',choices=['ANALOG','DIG'],default='0')
+parser.add_argument('--Coupling',help='Coupling mode for mod5761 test. 1 = DC, 0 = AC',choices=['1','0','DC','AC'],default='0')
+
+args = parser.parse_args()
+
+commonFlexRIOTests = "TP_FlexRIO_onlyResources.*:TP_FlexRIO_noModule.*"
+commonCRIOTests = "TP_cRIO_PBP.*:TP_cRIO_DAQ.*"
+
+if (args.RIODevice == "9159"):
+    commonTests = commonCRIOTests
+else:
+    commonTests = commonFlexRIOTests
+
+tests_5761 = "TP_FlexRIO_mod5761.*"
+tests_6581 = "TP_FlexRIO_mod6581.*"
+errorTests = "TP_errorTests.*"
+
+tests = {"ANALOG": commonTests+":"+"tests_5761"+":"+"TP_FlexRIO_perf.*",
+        "DIG": commonTests+":"+"tests_6581",
+        "0": commonTests}
+
+
 f = open('ErrorFile', 'w')
-
-functionalTests_5761 = ["TP_FlexRIO_onlyResources.CPUDAQ:TP_FlexRIO_onlyResources.CPUIMAQ:TP_FlexRIO_onlyResources.onlyResources:"+
-                        "TP_FlexRIO_noModule.functional:TP_FlexRIO_mod5761.functional"]
-                        # :TP_FlexRIO_mod5761_perf.functional"]
-
-functionalTests_6581 = ["TP_FlexRIO_onlyResources.CPUDAQ:TP_FlexRIO_onlyResources.CPUIMAQ:TP_FlexRIO_onlyResources.onlyResources:"+
-                        "TP_FlexRIO_noModule.functional:TP_FlexRIO_mod6581.functional"]
-
-tests_5761 = ["TP_FlexRIO_onlyResources.*:TP_FlexRIO_noModule.*:TP_FlexRIO_mod5761.*"]
-              # TP_FlexRIO_mod5761_perf.*"]
-              
-tests_6581 = ["TP_FlexRIO_onlyResources.*:TP_FlexRIO_noModule.*:TP_FlexRIO_mod6581.*"]
-
-tests_1483 = ["TP_FlexRIO_onlyResources.*:TP_FlexRIO_noModule.*"]
-
-# PXIe-7966R device + NI5761 module
-if(RIODevice=="7966" and RIOSerial=="0x0177A2AD" and int(allTests)):
-    for test in tests_5761:
-        if os.system("env -S RIOSerial="+RIOSerial+" env RIODevice="+RIODevice+" env Coupling="+Coupling+
-                     " ./driver-irio --gtest_filter="+test+iterations+shuffleAndBreak):
-            f.write(test+"\n")
-elif (RIODevice=="7966" and RIOSerial=="0x0177A2AD" and not(int(allTests))):
-     for test in functionalTests_5761:
-        if os.system("env -S RIOSerial="+RIOSerial+" env RIODevice="+RIODevice+" env Coupling="+Coupling+
-                     " ./driver-irio --gtest_filter="+test+iterations+shuffleAndBreak):
-            f.write(test+"\n")
-            
-
-# PXIe-7965R device + NI6581 module            
-if(RIODevice=="7965" and RIOSerial=="0x01666C59" and int(allTests)):
-    for test in tests_6581:
-        if os.system("env -S RIOSerial="+RIOSerial+" env RIODevice="+RIODevice+
-                     " ./driver-irio --gtest_filter="+test+iterations+shuffleAndBreak):
-            f.write(test+"\n")
-elif (RIODevice=="7965" and RIOSerial=="0x01666C59" and not(int(allTests))):
-     for test in functionalTests_6581:
-        if os.system("env -S RIOSerial="+RIOSerial+" env RIODevice="+RIODevice+
-                     " ./driver-irio --gtest_filter="+test+iterations+shuffleAndBreak):
-            f.write(test+"\n")
-
-
-# PXIe-7966R device + NI1483 module have to be executed manually 
-
+if (int(args.allTests)):
+    os.system("env -S RIOSerial="+args.RIOSerial+" env RIODevice="+args.RIODevice+" env Coupling="+args.Coupling+
+              " ./driver-irio --gtest_filter="+tests[args.AM]+":"+errorTests+iterations+shuffleAndBreak)
+else:
+    os.system("env -S RIOSerial="+args.RIOSerial+" env RIODevice="+args.RIODevice+" env Coupling="+args.Coupling+
+              " ./driver-irio --gtest_filter="+tests[args.AM]+iterations+shuffleAndBreak)
 
 f.close()
 
