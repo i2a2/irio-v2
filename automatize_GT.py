f2b014466f3dfd8ebb42ecda6b6317cddf3994bb
Jc Villanueva <josvilmar99@gmail.com>
2022-06-23 13:13:03 +0200

    Improvement of Python file. Now number of iterations can be passed as a parameter and the script also compile the project. Moreover, FlexRIO performance tests are executed on all 7966R devices, not only on the connected to the NI5761 adapter module

--- automatize_GT.py
+++ automatize_GT.py
@@ -4,12 +4,9 @@ import sys
 import argparse
 
 # It is mandatory to compile the project before execute this script
-#os.system("mvn clean compile -D coverage")
+os.system("mvn clean compile -D coverage")
 
 os.chdir(os.getcwd()+"/target/test/c++/irioTests/")
-
-# Number of iterations
-iterations = " --gtest_repeat=1"
 
 shuffleAndBreak = " --gtest_break_on_failure --gtest_shuffle"
 
@@ -19,11 +16,18 @@ parser.add_argument('--RIODevice',help='RIO device model. Use $lsrio command to 
 parser.add_argument('--RIOSerial',help='RIO device serial number. Use $lsrio command to display it',required='True')
 parser.add_argument('--allTests',help='Decide if run only funcional tests or funcional tests and error tests. 0 functional ; 1 functional+errors',choices=['1','0'],default='0')
 parser.add_argument('--AM',help='Indicate which adapter module is used. It is mandatory that the adapter module match with the RIODevice and RIOSerial used.',choices=['ANALOG','DIG'],default='0')
+parser.add_argument('--iterations',help='Select number of iterations of the tests. Pass a negative number to run indefinitely.',default='1')
 parser.add_argument('--Coupling',help='Coupling mode for mod5761 test. 1 = DC, 0 = AC',choices=['1','0','DC','AC'],default='0')
 
 args = parser.parse_args()
 
+# Number of iterations
+iterations = " --gtest_repeat="+args.iterations
+
 commonFlexRIOTests = "TP_FlexRIO_onlyResources.*:TP_FlexRIO_noModule.*"
+if (args.RIODevice=="7966"):
+    commonFlexRIOTests += ":TP_FlexRIO_perf.*"
+
 commonCRIOTests = "TP_cRIO_PBP.*:TP_cRIO_DAQ.*"
 
 if (args.RIODevice == "9159"):
@@ -35,10 +39,9 @@ tests_5761 = "TP_FlexRIO_mod5761.*"
 tests_6581 = "TP_FlexRIO_mod6581.*"
 errorTests = "TP_errorTests.*"
 
-tests = {"ANALOG": commonTests+":"+"tests_5761"+":"+"TP_FlexRIO_perf.*",
+tests = {"ANALOG": commonTests+":"+"tests_5761",
         "DIG": commonTests+":"+"tests_6581",
         "0": commonTests}
-
 
 f = open('ErrorFile', 'w')
 if (int(args.allTests)):
@@ -52,4 +55,3 @@ f.close()
 
 os.system('mv ErrorFile ../../../../')
 print("See file ErrorFile on that directory to check if a test has failed")
-
