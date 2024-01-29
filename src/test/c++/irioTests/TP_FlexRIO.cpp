98554281a16e284128eb41ad38bd0b6246ff81ac
David-Andrino <davidandrino2002@gmail.com>
2024-01-29 13:36:08 +0100

    Refactor previous tests and create ResourcesMAXIO and ResourcesMissing

--- src/test/c++/irioTests/TP_FlexRIO.cpp
+++ src/test/c++/irioTests/TP_FlexRIO.cpp
@@ -41,55 +41,195 @@ TEST(FlexRIO, InitClose) {
  * configuration.
  *
  * Implemented in tests:
- * InitCloseCPUDAQ
+ * - ResourcesCPUDAQ
+ * - ResourcesCPUIMAQ
+ * - ResourcesMAXIO
+ * - ResourcesMissing
  */
-TEST(FlexRIO, InitCloseCPUDAQ) {
+TEST(FlexRIO, ResourcesCPUDAQ) {
     int verbose_test = std::stoi(TestUtilsIRIO::getEnvVar("VerboseTest"));
 
     irioDrv_t drv;
     initDriver(std::string("FlexRIO_CPUDAQ_"), &drv);
 
+	irioResources_t res;
+	getResources(&drv, &res);
+
     // Expected resources:
     // 2 DMAs
-    int dmas = (drv.DMATtoHOSTNo.found ? drv.DMATtoHOSTNo.value : 0) + (drv.DMATtoGPUNo.found ? drv.DMATtoGPUNo.value : 0);
-    if (verbose_test) cout << "[TEST] Found " << dmas << " DMAs. Expected 2" << endl;
-    EXPECT_EQ(dmas , 2);
+    if (verbose_test) cout << "[TEST] Found " << res.DMA << " DMAs. Expected 2" << endl;
+    EXPECT_EQ(res.DMA, 2);
     // 2 AI
-	int AIs = getResourceCount(drv.enumAnalogInput, drv.max_analoginputs);
-	if (verbose_test) cout << "[TEST] Found " << AIs << " AIs. Expected 2" << endl;
-	EXPECT_EQ(AIs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.AI << " AIs. Expected 2" << endl;
+	EXPECT_EQ(res.AI, 2);
     // 2 AO
-	int AOs = getResourceCount(drv.enumAnalogOutput, drv.max_analogoutputs);
-	if (verbose_test) cout << "[TEST] Found " << AOs << " AOs. Expected 2" << endl;
-	EXPECT_EQ(AOs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.AO << " AOs. Expected 2" << endl;
+	EXPECT_EQ(res.AO, 2);
     // 2 auxAI
-	int auxAIs = getResourceCount(drv.enumauxAI, drv.max_auxanaloginputs);
-	if (verbose_test) cout << "[TEST] Found " << auxAIs << " auxAIs. Expected 2" << endl;
-	EXPECT_EQ(auxAIs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.auxAI << " auxAIs. Expected 2" << endl;
+	EXPECT_EQ(res.auxAI, 2);
     // 2 auxAO
-	int auxAOs = getResourceCount(drv.enumauxAO, drv.max_auxanalogoutputs);
-	if (verbose_test) cout << "[TEST] Found " << auxAOs << " auxAOs. Expected 2" << endl;
-	EXPECT_EQ(auxAOs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.auxAO << " auxAOs. Expected 2" << endl;
+	EXPECT_EQ(res.auxAO, 2);
     // 2 DI
-	int DIs = getResourceCount(drv.enumDigitalInput, drv.max_digitalsinputs);
-	if (verbose_test) cout << "[TEST] Found " << DIs << " DIs. Expected 2" << endl;
-	EXPECT_EQ(DIs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.DI << " DIs. Expected 2" << endl;
+	EXPECT_EQ(res.DI, 2);
     // 2 DO
-	int DOs = getResourceCount(drv.enumDigitalOutput, drv.max_digitalsoutputs);
-	if (verbose_test) cout << "[TEST] Found " << DOs << " DOs. Expected 2" << endl;
-	EXPECT_EQ(DOs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.DO << " DOs. Expected 2" << endl;
+	EXPECT_EQ(res.DO, 2);
     // 2 auxDI
-	int auxDIs = getResourceCount(drv.enumauxDI, drv.max_auxdigitalsinputs);
-	if (verbose_test) cout << "[TEST] Found " << auxDIs << " auxDIs. Expected 2" << endl;
-	EXPECT_EQ(auxDIs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.auxDI << " auxDIs. Expected 2" << endl;
+	EXPECT_EQ(res.auxDI, 2);
     // 2 auxDO
-	int auxDOs = getResourceCount(drv.enumauxDO, drv.max_auxdigitalsoutputs);
-	if (verbose_test) cout << "[TEST] Found " << auxDOs << " auxDOs. Expected 2" << endl;
-	EXPECT_EQ(auxDOs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.auxDO << " auxDOs. Expected 2" << endl;
+	EXPECT_EQ(res.auxDO, 2);
     // 2 SG
-	int SGs = drv.NoOfSG;
-	if (verbose_test) cout << "[TEST] Found " << SGs << " SGs. Expected 2" << endl;
-	EXPECT_EQ(SGs, 2);
+	if (verbose_test) cout << "[TEST] Found " << res.SG << " SGs. Expected 2" << endl;
+	EXPECT_EQ(res.SG, 2);
+
+    closeDriver(&drv);
+}
+TEST(FlexRIO, ResourcesIMAQ) {
+    int verbose_test = std::stoi(TestUtilsIRIO::getEnvVar("VerboseTest"));
+
+    irioDrv_t drv;
+    initDriver(std::string("FlexRIO_CPUIMAQ_"), &drv);
+
+	irioResources_t res;
+	getResources(&drv, &res);
+
+    // Expected resources
+	// 1 IMAQ DMA
+	if (verbose_test) cout << "[TEST] Found " << res.DMA << " IMAQ DMAs. Expected 1" << endl;
+	EXPECT_EQ(res.DMA, 1);
+	// 1 CLConfig
+	if (verbose_test) cout << "[TEST] CLConfig " << (res.CLConfig ? "found" : "not found") << endl;
+	EXPECT_EQ(res.CLConfig, 1);
+	// 1 CLUART
+	if (verbose_test) cout << "[TEST] CLUART " << (res.CLUART ? "found" : "not found") << endl;
+	EXPECT_EQ(res.CLUART, 1);
+    // 2 auxAI
+	if (verbose_test) cout << "[TEST] Found " << res.auxAI << " auxAIs. Expected 2" << endl;
+	EXPECT_EQ(res.auxAI, 2);
+    // 2 auxAO
+	if (verbose_test) cout << "[TEST] Found " << res.auxAO << " auxAOs. Expected 2" << endl;
+	EXPECT_EQ(res.auxAO, 2);
+    // 2 DI
+	if (verbose_test) cout << "[TEST] Found " << res.DI << " DIs. Expected 2" << endl;
+	EXPECT_EQ(res.DI, 2);
+    // 2 DO
+	if (verbose_test) cout << "[TEST] Found " << res.DO << " DOs. Expected 2" << endl;
+	EXPECT_EQ(res.DO, 2);
+    // 2 auxDI
+	if (verbose_test) cout << "[TEST] Found " << res.auxDI << " auxDIs. Expected 2" << endl;
+	EXPECT_EQ(res.auxDI, 2);
+    // 2 auxDO
+	if (verbose_test) cout << "[TEST] Found " << res.auxDO << " auxDOs. Expected 2" << endl;
+	EXPECT_EQ(res.auxDO, 2);
+
+    closeDriver(&drv);
+}
+TEST(FlexRIO, ResourcesMAXIO) {
+    int verbose_test = std::stoi(TestUtilsIRIO::getEnvVar("VerboseTest"));
+
+    irioDrv_t drv;
+    initDriver(std::string("FlexRIOonlyResources_"), &drv);
+
+	irioResources_t res;
+	getResources(&drv, &res);
+
+    // Expected resources:
+    // 1 DMAs
+    if (verbose_test) cout << "[TEST] Found " << res.DMA << " DMAs. Expected 1" << endl;
+    EXPECT_EQ(res.DMA , 1);
+    // 2 AO
+	if (verbose_test) cout << "[TEST] Found " << res.AO << " AOs. Expected 2" << endl;
+	EXPECT_EQ(res.AO, 2);
+    // 16 auxAI
+	if (verbose_test) cout << "[TEST] Found " << res.auxAI << " auxAIs. Expected 16" << endl;
+	EXPECT_EQ(res.auxAI, 16);
+    // 16 auxAO
+	if (verbose_test) cout << "[TEST] Found " << res.auxAO << " auxAOs. Expected 16" << endl;
+	EXPECT_EQ(res.auxAO, 16);
+    // 16 auxDI
+	if (verbose_test) cout << "[TEST] Found " << res.auxDI << " auxDIs. Expected 16" << endl;
+	EXPECT_EQ(res.auxDI, 16);
+    // 16 auxDO
+	if (verbose_test) cout << "[TEST] Found " << res.auxDO << " auxDOs. Expected 16" << endl;
+	EXPECT_EQ(res.auxDO, 16);
+    // 54 DI
+	if (verbose_test) cout << "[TEST] Found " << res.DI << " DIs. Expected 54" << endl;
+	EXPECT_EQ(res.DI, 54);
+    // 54 DO
+	if (verbose_test) cout << "[TEST] Found " << res.DO << " DOs. Expected 54" << endl;
+	EXPECT_EQ(res.DO, 54);
+    // 2 SG
+	if (verbose_test) cout << "[TEST] Found " << res.SG << " SGs. Expected 2" << endl;
+	EXPECT_EQ(res.SG, 2);
+
+    closeDriver(&drv);
+}
+TEST(FlexRIO, ResourcesMissing) {
+	irioDrv_t drv;
+
+	// Manual initialization to load a failResources file and test for error instead of success
+    int st = IRIO_success;
+
+    int verbose_init = std::stoi(TestUtilsIRIO::getEnvVar("VerboseInit"));
+    int verbose_test = std::stoi(TestUtilsIRIO::getEnvVar("VerboseTest"));
+    string RIODevice = TestUtilsIRIO::getEnvVar("RIODevice");
+    string RIOSerial = TestUtilsIRIO::getEnvVar("RIOSerial");
+
+    string NIRIOmodel = "PXIe-" + RIODevice + "R";
+    string bitfileName = "FlexRIOonlyResources_" + RIODevice;
+    string filePath = "../resources/failResources/" + RIODevice + "/";
+    string testName = ("Test_" + bitfileName);
+
+    TStatus status;
+    irio_initStatus(&status);
+
+    if (verbose_test) cout << "[TEST] Initializing driver with bitfile \"failResources/" << bitfileName << "\"" << endl;
+    st = irio_initDriver(testName.c_str(), RIOSerial.c_str(),
+                         NIRIOmodel.c_str(), bitfileName.c_str(), "V1.2",
+                         verbose_init, filePath.c_str(), filePath.c_str(), &drv,
+                         &status);
+    if (verbose_test) cout << "[TEST] Driver initialized " << ((st == IRIO_success) ? "successfully (Error)" : "unsuccessfully (Expected)") << endl;
+    EXPECT_NE(st, IRIO_success);
+
+	// Test for errors:
+	// Error finding AO1Enable
+
+	irioResources_t res;
+	getResources(&drv, &res);
+
+    // Expected resources:
+    // 1 AO + TODO: Error finding AO1Enable : ResourceNotFoundWarning
+	if (verbose_test) cout << "[TEST] Found " << res.AO << " AOs. Expected 1" << endl;
+	EXPECT_EQ(res.AO, 1);
+    // 16 auxAI
+	if (verbose_test) cout << "[TEST] Found " << res.auxAI << " auxAIs. Expected 16" << endl;
+	EXPECT_EQ(res.auxAI, 16);
+    // 16 auxAO
+	if (verbose_test) cout << "[TEST] Found " << res.auxAO << " auxAOs. Expected 16" << endl;
+	EXPECT_EQ(res.auxAO, 16);
+    // 16 auxDI
+	if (verbose_test) cout << "[TEST] Found " << res.auxDI << " auxDIs. Expected 16" << endl;
+	EXPECT_EQ(res.auxDI, 16);
+    // 16 auxDO
+	if (verbose_test) cout << "[TEST] Found " << res.auxDO << " auxDOs. Expected 16" << endl;
+	EXPECT_EQ(res.auxDO, 16);
+    // 54 DI
+	if (verbose_test) cout << "[TEST] Found " << res.DI << " DIs. Expected 54" << endl;
+	EXPECT_EQ(res.DI, 54);
+    // 54 DO
+	if (verbose_test) cout << "[TEST] Found " << res.DO << " DOs. Expected 54" << endl;
+	EXPECT_EQ(res.DO, 54);
+    // 0 SG + TODO: Error finding SGFref0 and SGSignalType1
+	if (verbose_test) cout << "[TEST] Found " << res.SG << " SGs. Expected 0" << endl;
+	EXPECT_EQ(res.SG, 2);
+    // 0 DMAs + TODO: Error finding DMATtoHOSTSamplingRate0
+    if (verbose_test) cout << "[TEST] Found " << res.DMA << " DMAs. Expected 0" << endl;
+    EXPECT_EQ(res.DMA , 0);
 
     closeDriver(&drv);
 }
